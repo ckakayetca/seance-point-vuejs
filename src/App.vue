8e2cc03625cd1e5d77490a7ea4d79e37696189c2
ckakayetca <113856594+ckakayetca@users.noreply.github.com>
2023-11-25 23:22:27 +0200

    implement Login + Register page + styling

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <script setup>
-import Home from './views/Home.vue';
 import CoreHeader from './components/core/CoreHeader.vue'
 import CoreFooter from './components/core/CoreFooter.vue'
+import Register from './views/auth/Register.vue';
+import Home from './views/Home.vue';
 import NewSeance from './views/seances/NewSeance.vue';
-
+import Login from './views/auth/Login.vue';
 </script>
 
 <template>
   <CoreHeader></CoreHeader>
   <div class="container">
-    <NewSeance></NewSeance>
+    <Register></Register>
+    <Login></Login>
   </div>
   <CoreFooter></CoreFooter>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,113 @@
+<script>
+import useVuelidate from '@vuelidate/core';
+import * as vuelidators from '@vuelidate/validators'
+import FormInput from '../../components/shared/FormInput.vue';
+
+const userPassRegex = /^\w+$/;
+function matchesRegex(regex) {
+    return (email) => {
+        return regex.test(email);
+    }
+}
+
+export default {
+    components: { FormInput },
+    setup() {
+        return {
+            v$: useVuelidate(),
+        }
+    },
+    data() {
+        return {
+            formData: {
+                username: '',
+                pass: '',
+            }
+        }
+    },
+    validations() {
+        return {
+            formData: {
+                username: {
+                    type: String,
+                    required: true,
+                    matchesRegex: vuelidators.helpers.withMessage('Invalid input!', matchesRegex(userPassRegex)),
+                    minLength: vuelidators.minLength(4),
+                },
+                pass: {
+                    type: String,
+                    required: true,
+                    matchesRegex: vuelidators.helpers.withMessage('Invalid input!', matchesRegex(userPassRegex)),
+                    minLength: vuelidators.minLength(5),
+                },
+            }
+        }
+    },
+    methods: {
+        async onSubmit() {
+            const res = await this.v$.$validate();
+
+            if(!res) {
+                console.log('Form is invalid! Interrupting submit');
+                return
+            }
+            console.log(this.formData);
+        }
+    }
+
+}
+</script>
+
+<template>
+    <!-- Username is required -->
+    <!-- Username must match this regex /^\w+$/ -->
+    <!-- Username should be at least 4 characters -->
+
+    <!-- Password is required -->
+    <!-- Password must match this regex /^\w+$/ -->
+    <!-- Password should be at least 5 characters -->
+
+    <form action="" class="login" @submit.prevent="onSubmit">
+        <h1>Login Page</h1>
+
+        <fieldset>
+            <FormInput field="username" label="Username" required v-model="formData.username" :v$="v$"></FormInput>
+
+            <FormInput field="pass" type="password" label="Password" required v-model="formData.pass" :v$="v$"></FormInput>
+
+            <div class="form-control">
+                <button class="button">Log In!</button>
+            </div>
+        </fieldset>
+    </form>
+
+    <p class="signup">
+        Don't have an account?
+        <a href="#" class="button"> Sign Up! </a>
+    </p>
+</template>
+
+
+<style>
+
+.login {
+    margin: 1em auto;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    width: 50%
+}
+
+.login fieldset {
+    margin: 3em;
+}
+
+.signup {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+
+</style>
