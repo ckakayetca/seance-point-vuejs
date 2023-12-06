98e507fab3bbbfc4623ef5e09f606658d345f14c
ckakayetca <113856594+ckakayetca@users.noreply.github.com>
2023-12-06 14:01:48 +0200

    register loader component as global

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import './style.css'
 import App from './App.vue'
 import router from './routes/index'
 import { createPinia } from 'pinia'
+import AppLoader from './components/shared/AppLoader.vue'
 
 const pinia = createPinia();
 const app = createApp(App);
 
+app.component('AppLoader', AppLoader)
 app.use(pinia);
 app.use(router);
 app.mount('#app');
